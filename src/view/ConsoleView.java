ab0c667418a8af983fc0ea137931d40174b20039
Nguyen Quoc Khang <10423055@student.vgu.edu.vn>
2025-11-20 12:28:39 +0700

    Fixed wrongly commited files

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,52 +1,68 @@
 package view;
 import controller.TransactionController;
-import java.util.*;
+import model.Transaction;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+
 public class ConsoleView {
     private TransactionController controller = new TransactionController();
-    private Scanner sc =  new Scanner(System.in);
+    private Scanner sc = new sc(System.in);
 
-
-    public void start(){
-        while (true) { 
-            
+    public void start() {
+        while (true) {
             System.out.println("\nExpense Tracker");
             System.out.println("1. Add Transaction");
             System.out.println("2. View All Transactions");
             System.out.println("3. Delete Transaction");
             System.out.println("4. Exit");
-            System.out.println("Choose: ");
+            System.out.print("Choose: ");
 
             int choice = sc.nextInt();
             sc.nextLine();
-            
-            switch(choice){
+
+            switch (choice) {
                 case 1 -> addTransaction();
-                case 2 -> showTransaction();
+                case 2 -> showTransactions();
                 case 3 -> deleteTransaction();
-                case 4 -> {return; }
-
+                case 4 -> { return; }
             }
-            
         }
-
-
     }
 
-    private void addTransaction(){
-        
+    private void addTransaction() {
+        System.out.print("Date (YYYY-MM-DD): ");
+        LocalDateTime dateTime = LocalDateTime.parse(sc.nextLine());
+
+        System.out.print("Amount: ");
+        double amount = Double.parseDouble(sc.nextLine());
+
+        System.out.print("Category: ");
+        String cat = sc.nextLine();
+
+        System.out.print("Note: ");
+        String note = sc.nextLine();
+
+        controller.addTransaction(dateTime, amount, cat, note);
+        System.out.println("Added!");
     }
 
-    private void showTransaction(){
-        
+    private void showTransactions() {
+        for (Transaction t : controller.getAllTransactions()) {
+            System.out.println(t.getId() + " | " + t.getDateTime() + " | " + t.getAmount() 
+                               + " | " + t.getCategory() + " | " + t.getdescription());
+        }
     }
 
-    private void deleteTransaction(){
-        
+    private void deleteTransaction() {
+        System.out.print("Enter ID to delete: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        if (controller.deleteTransaction(id)) {
+            System.out.println("Deleted.");
+        } else {
+            System.out.println("ID not found.");
+        }
     }
-
-
 }
-    
-
-
-
